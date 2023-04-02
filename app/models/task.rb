dd69224d025102159e777eeefde792acdd50ee59
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-04-03 08:15:04 +0900

    モデルに検証を追加し, タスクの名前が nul または 30 文字以内でないものを登録できないようにした

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,12 @@
 class Task < ApplicationRecord
+  # name に 必須のデータはちゃんと入っているか, タスクの名前が 30 文字以内であるか.
+  validates :name, presence: true, length: { maximum: 30 }
+  # タスクの name 属性の値にカンマは含まれていないか
+  validate :validate_name_not_including_comma
+
+  private
+
+  def validate_name_not_including_comma
+    errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
 end
