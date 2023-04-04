36edd3311935740b89883597769c8898357c3157
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-04-04 22:55:54 +0900

    ログイン機能を実装した

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    # フォームから送られてきたメールアドレスでユーザを検索検索する
+    user = User.find_by(email: session_params[:email])
+
+    # ユーザが見つかった場合は, authenticate メソッドで送られてきたパスワードの認証を行う
+    if user&.authenticate(session_params[:password])
+      # 認証に成功した場合に, セッションに user_id を格納する
+      session[:user_id] = user.id
+      redirect_to root_url, notice: 'ログインしました。'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
+end
